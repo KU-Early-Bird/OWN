cc317fe80cd5169bbaa5380775c0b6c2ca84c72b
c-jeongyyun <99jenny0614@naver.com>
2022-05-31 19:08:57 +0900

    feat: 트로피 버튼 누르면 side nav 등장하도록 구현

--- app/src/main/java/com/example/own/MainActivity.kt
+++ app/src/main/java/com/example/own/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.own
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.example.own.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initLayer()
+    }
+
+    private fun initLayer() {
+        // achieve 버튼 클릭 시 drawerLayout open
+
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_bar,menu)
+        return true
+    }
+
+    // achieve 아이콘 클릭 시 achieve nav open
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.achieveIcon -> {
+                binding.drawerLayout.openDrawer(binding.drawerNav)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
