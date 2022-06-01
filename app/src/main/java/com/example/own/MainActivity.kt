ab6215e39c0d8dc970e230e37ab62304828550e1
c-jeongyyun <99jenny0614@naver.com>
2022-06-01 16:51:41 +0900

    feat: 개수에 맞게 노른자 이미지 grid 출력

--- app/src/main/java/com/example/own/MainActivity.kt
+++ app/src/main/java/com/example/own/MainActivity.kt
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
+import android.widget.GridView
+import android.widget.TextView
 import com.example.own.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
+    var ownwanDays = 127
+    var yolkNum =0
+    var level=0
+    val YOLK_GRID_SIZE = 30
+    var yolks = ArrayList<Int>(YOLK_GRID_SIZE)
+    lateinit var yolkGridAdapter:YolkGridAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -17,10 +25,43 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initLayer() {
-        // achieve 버튼 클릭 시 drawerLayout open
+        level = ownwanDays/30
+        yolkNum = ownwanDays%30
+
+        printAchieveSection()
 
 
+        initYolkArr()
+        yolkGridAdapter = YolkGridAdapter(this,yolks)
+        binding.drawerNav.findViewById<GridView>(R.id.yolkGrid).adapter = yolkGridAdapter
 
+    }
+
+    private fun initYolkArr(){
+        for(i: Int in 0..(YOLK_GRID_SIZE-1)){
+            if(i<yolkNum){
+                yolks.add(1)
+            }else{
+                yolks.add(0)
+            }
+        }
+    }
+
+    // achieve 화면 출력 함수 - 함수명 바꿀 것 (텍스트 내용만 변경함)
+    private fun printAchieveSection(){
+        // 오늘 운동한 날짜
+        val dayCnt = findViewById<TextView>(R.id.dayCnt)
+        dayCnt.text = ownwanDays.toString()
+
+        // 레벨
+        val levelView = findViewById<TextView>(R.id.levelNum)
+        levelView.text = "Lv."+ level.toString()
+        // level grid에도 반영
+
+        // 노른자 개수
+        val yolkNumView = findViewById<TextView>(R.id.yolkNum)
+        yolkNumView.text = yolkNum.toString() +"/" + YOLK_GRID_SIZE.toString()
+        // yolk grid에도 반영
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
