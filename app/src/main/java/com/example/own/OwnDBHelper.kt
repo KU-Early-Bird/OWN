093b56531f645e1f66052b59e61d25b9a0647ee3
c-jeongyyun <99jenny0614@naver.com>
2022-06-08 03:14:59 +0900

    style: achieve table 관련 변수명 변경

--- app/src/main/java/com/example/own/OwnDBHelper.kt
+++ app/src/main/java/com/example/own/OwnDBHelper.kt
@@ -6,13 +6,10 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.util.Log
 import android.widget.Toast
-import androidx.fragment.app.FragmentActivity
 import java.text.SimpleDateFormat
 import java.util.*
 import com.example.own.Diary.DiaryData
 
-//class OwnDBHelper(val context: FragmentActivity?) : SQLiteOpenHelper(context, DB_NAME, null, DB_VERSION) {
-//=======
 
 class OwnDBHelper(val context: Context) : SQLiteOpenHelper(context, DB_NAME, null, DB_VERSION) {
     companion object{
@@ -20,7 +17,7 @@ class OwnDBHelper(val context: Context) : SQLiteOpenHelper(context, DB_NAME, nul
         val DB_VERSION =1
 
         // about achieve table
-        val TABLE_NAME = "achieve"
+        val ACHIEVE_TABLE_NAME = "achieve"
         val LAST_UPDATE ="lastUpdate"
         val OWNWAN_DAYS ="ownwanDays"
         val DID_COMPLETE = "didComplete"
@@ -42,13 +39,12 @@ class OwnDBHelper(val context: Context) : SQLiteOpenHelper(context, DB_NAME, nul
     // 데이터 베이스 생성시 테이블 없다면 생성
     override fun onCreate(db: SQLiteDatabase?) {
 
-        var create_table ="create table if not exists $TABLE_NAME(" +
-
+        var create_achieve_table ="create table if not exists $ACHIEVE_TABLE_NAME(" +
                 "$LAST_UPDATE date primary key default (date('now')), " +
                 "$OWNWAN_DAYS integer," +
                 "$DID_COMPLETE tinyint );"
 
-        db!!.execSQL(create_table) // SQL에서 실행해라! - 테이블 생성
+        db!!.execSQL(create_achieve_table) // SQL에서 실행해라! - 테이블 생성
 
         var create_diary_table = "create table if not exists $DIARY_TABLE_NAME(" +
                 "DID integer primary key autoincrement, " +
@@ -59,24 +55,18 @@ class OwnDBHelper(val context: Context) : SQLiteOpenHelper(context, DB_NAME, nul
         Log.e("db" , "table had been made")
     }
 
-//    public  fun initData(){
-//        val achieveData = readCurRecord()
-//        if(achieveData.lastUpdateDate == null){
-//            insertRecord(0,0)
-//        }
-//    }
 
     // 테이블 업그레이드 시 실행 내용
     override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
-        val drop_table = "drop table if exists $TABLE_NAME;"
+        val drop_table = "drop table if exists $ACHIEVE_TABLE_NAME;"
         db!!.execSQL(drop_table)
         onCreate(db)
     }
 
     // 현재 테이블 내용 가져오기 - 반환해야할 것 : 날짜 & ownwanDays (어차피 한줄)
-    public fun readCurRecord():AchieveTableData{
+    public fun readAchieve():AchieveTableData{
         // 질의문으로 데이터 베이스 모든 내용 가져오기
-        val strSql = "select * from $TABLE_NAME;"
+        val strSql = "select * from $ACHIEVE_TABLE_NAME;"
         val db = readableDatabase
         val cursor = db.rawQuery(strSql,null) // 조건이 없어서 null
 
@@ -120,7 +110,7 @@ class OwnDBHelper(val context: Context) : SQLiteOpenHelper(context, DB_NAME, nul
         val db = writableDatabase
 
         // 오류나면 -1 반환,
-        val flag = db.insert(TABLE_NAME, null, values)>0
+        val flag = db.insert(ACHIEVE_TABLE_NAME, null, values)>0
 
         // 삽입 명령어 실행하면 무조건 db.close해야함 (성공 실패 관계 없이)
         db.close()
@@ -128,23 +118,19 @@ class OwnDBHelper(val context: Context) : SQLiteOpenHelper(context, DB_NAME, nul
         return flag
     }
 
-    public fun deleteRecord(){
-        val strSql = "delete from $TABLE_NAME;"
+    public fun deleteAchieve(){
+        val strSql = "delete from $ACHIEVE_TABLE_NAME;"
         val db = writableDatabase
-//        val cursor = db.rawQuery(strSql,null)
-        var flag = db.delete(TABLE_NAME,null,null)
-//        if(flag==-1)
-//            Toast.makeText(context,"fail",Toast.LENGTH_LONG).show()
-//        else
-//            Toast.makeText(context,"success",Toast.LENGTH_LONG).show()
+        var flag = db.delete(ACHIEVE_TABLE_NAME,null,null)
+        if(flag==-1)
+            Toast.makeText(context,"fail",Toast.LENGTH_SHORT).show()
 
-//        cursor.close()
         db.close()
     }
 
     // 테이블 내용 업데이트 하기
-    public fun updateRecord(ownwanDays:Int,didComplete: Boolean){
-        deleteRecord()
+    public fun updateAchieve(ownwanDays:Int,didComplete: Boolean){
+        deleteAchieve()
         var didCompleteFlag = 0
         if(didComplete) didCompleteFlag=1 else didCompleteFlag=0
         insertRecord(ownwanDays,didCompleteFlag)
