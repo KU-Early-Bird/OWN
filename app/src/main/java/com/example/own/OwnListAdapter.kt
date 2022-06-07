cbb191d22013dd1b4e5cedde5a88822961d1b33b
c-jeongyyun <99jenny0614@naver.com>
2022-06-07 18:56:37 +0900

    feat: ownList 뷰 및 이벤트 수정 - 현재 날짜인 경우에만 이벤트 부여 - 아직 하지 않은 운동인 경우 view 다르게 보이기 - 색상테마 수정

--- app/src/main/java/com/example/own/OwnListAdapter.kt
+++ app/src/main/java/com/example/own/OwnListAdapter.kt
@@ -1,15 +1,23 @@
 package com.example.own
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.own.databinding.DailyWorkoutRowBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
-class OwnListAdapter(val items:ArrayList<OwnListData>) : RecyclerView.Adapter<OwnListAdapter.ViewHolder>() {
+class OwnListAdapter(val items:ArrayList<OwnListData>, val today: GregorianCalendar?) : RecyclerView.Adapter<OwnListAdapter.ViewHolder>() {
+
+     interface OnItemClickListener : View.OnClickListener {
+         override fun onClick(p0: View?)
+     }
+
+    val onItemClickListener:OnItemClickListener ?= null
 
     inner class ViewHolder(val binding: DailyWorkoutRowBinding) : RecyclerView.ViewHolder(binding.root) {
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -20,14 +28,28 @@ class OwnListAdapter(val items:ArrayList<OwnListData>) : RecyclerView.Adapter<Ow
 
     override fun onBindViewHolder(holder: ViewHolder, pos: Int) {
         holder.binding.apply {
-            // 아이템 리스트(<- 클릭 날짜마다 달라짐) 의 값들로 채워 넣기
-            // emoji
-            when(items[pos]){
-//                0->
-//                1->
-//                2->
+            if(items[pos].date.compareTo(today)==0){
+                ownListItem.setOnClickListener{onItemClickListener}
             }
+            // text
             ownListEmoji.setImageResource(R.drawable.ic_baseline_mood_24)
+
+            // emoji
+            if(items[pos].isDone){
+                when(items[pos]){
+    //                0->
+    //                1->
+    //                2->
+                }
+                ownListItem.setBackgroundResource(R.color.own_skyblue_darkBack)
+                ownListBodyPart.setBackgroundResource(R.color.own_skyblue_item_done)
+                ownListSet.setBackgroundResource(R.color.own_skyblue_item_done)
+            }else{
+                ownListItem.setBackgroundResource(R.color.own_skyblue_defaultBack)
+                ownListBodyPart.setBackgroundResource(R.color.own_yellow)
+                ownListSet.setBackgroundResource(R.color.own_yellow)
+                ownListEmoji.setImageResource(R.drawable.ic_baseline_check_box_outline_blank_24)
+            }
             ownListWorkoutName.text = items[pos].name
             ownListBodyPart.text = items[pos].bodyPart
             ownListSet.text = items[pos].set.toString()+"세트"
