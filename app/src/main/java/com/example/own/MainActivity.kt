d41a3b984029d6f8dc70f353d14ef335d48494f7
c-jeongyyun <99jenny0614@naver.com>
2022-06-07 16:20:58 +0900

    feat:상단바 이벤트 사라진것 다시 추가 - side화면 나타나도록
    Signed-off-by: c-jeongyyun <99jenny0614@naver.com>

--- app/src/main/java/com/example/own/MainActivity.kt
+++ app/src/main/java/com/example/own/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.own
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.own.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
@@ -28,5 +31,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.top_bar,menu)
         return true
+    }
+
+    // achieve 아이콘 클릭 시 achieve nav open
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        val drawerNav = findViewById<NavigationView>(R.id.drawerNav)
+        return when (item.itemId) {
+            R.id.achieveIcon -> {
+                drawerLayout.openDrawer(drawerNav)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
